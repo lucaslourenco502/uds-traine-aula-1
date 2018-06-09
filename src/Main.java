696ba5ca6cb5835fa77fc82ad4cd12af6a3deb7f
Lucas Lourenço <lucas-rafael2@hotmail.com>
2018-06-09 16:41:39 -0300

    Modificação arquivo3

--- src/Main.java
+++ src/Main.java
@@ -2,199 +2,199 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    float saldo = 15000;
-    float novosaldo;
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Categoria:");
-    int categoria = scanner.nextInt();
+        float saldo = 1500;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Categoria:");
+        int categoria = scanner.nextInt();
+        
+        switch (categoria) {
+            case 1: {
+                System.out.println("Pistols");
 
-    switch (categoria){
-        case 1:{
-            System.out.println("Pistols");
+                Scanner var = new Scanner(System.in);
+                System.out.println("Produto:");
+                int produtos = var.nextInt();
 
-            Scanner var = new Scanner(System.in);
-            System.out.println("Produto:");
-            int produtos = var.nextInt();
-
-            switch (produtos){
-                case 1: {
-                    System.out.println("Desert Eagle");
-                    novosaldo = saldo - 250;
-                    break;
+                switch (produtos) {
+                    case 1: {
+                        System.out.println("Desert Eagle");
+                        saldo -= 250;
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("Glock");
+                        saldo -= 150;
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("Revolver");
+                        saldo -= 200;
+                        break;
+                    }
+                    case 4: {
+                        System.out.println("Dual Berettas");
+                        saldo -= 300;
+                        break;
+                    }
+                    case 5: {
+                        System.out.println(".45");
+                        saldo -= 210;
+                        break;
+                    }
                 }
-                case 2: {
-                    System.out.println("Glock");
-                    novosaldo = saldo - 150;
-                    break;
-                }
-                case 3: {
-                    System.out.println("Revolver");
-                    novosaldo = saldo - 200;
-                    break;
-                }
-                case 4: {
-                    System.out.println("Dual Berettas");
-                    novosaldo = saldo - 300;
-                    break;
-                }
-                case 5: {
-                    System.out.println(".45");
-                    novosaldo = saldo - 210;
-                    break;
-                }
+                break;
             }
-            break;
-        }
 
-        case 2:{
-            System.out.println("Submachine guns");
+            case 2: {
+                System.out.println("Submachine guns");
 
 
-            Scanner var2 = new Scanner(System.in);
-            System.out.println("Produto:");
-            int produtos = var2.nextInt();
+                Scanner var2 = new Scanner(System.in);
+                System.out.println("Produto:");
+                int produtos = var2.nextInt();
 
-            switch (produtos) {
-                case 1: {
-                    System.out.println("P90");
-                    novosaldo = saldo - 500;
-                    break;
+                switch (produtos) {
+                    case 1: {
+                        System.out.println("P90");
+                        saldo -= 500;
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("UMP-45");
+                        saldo -= 400;
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("MAC-10");
+                        saldo -= 450;
+                        break;
+                    }
+                    case 4: {
+                        System.out.println("MP7");
+                        saldo -= 500;
+                        break;
+                    }
+                    case 5: {
+                        System.out.println("MP9");
+                        saldo -= 475;
+                        break;
+                    }
                 }
-                case 2: {
-                    System.out.println("UMP-45");
-                    novosaldo = saldo - 400;
-                    break;
-                }
-                case 3: {
-                    System.out.println("MAC-10");
-                    novosaldo = saldo - 450;
-                    break;
-                }
-                case 4: {
-                    System.out.println("MP7");
-                    novosaldo = saldo - 500;
-                    break;
-                }
-                case 5: {
-                    System.out.println("MP9");
-                    novosaldo = saldo - 475;
-                    break;
-                }
+                break;
             }
-            break;
-        }
 
-        case 3:{
-            System.out.println("Assault Rifles");
+            case 3: {
+                System.out.println("Assault Rifles");
 
-            Scanner var3 = new Scanner(System.in);
-            System.out.println("Produto:");
-            int produtos = var3.nextInt();
+                Scanner var3 = new Scanner(System.in);
+                System.out.println("Produto:");
+                int produtos = var3.nextInt();
 
-            switch (produtos) {
-                case 1: {
-                    System.out.println("M4A1");
-                    novosaldo = saldo - 800;
-                    break;
+                switch (produtos) {
+                    case 1: {
+                        System.out.println("M4A1");
+                        saldo -= 800;
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("FAMAS");
+                        saldo -= 900;
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("SCAR-H");
+                        saldo -= 1050;
+                        break;
+                    }
+                    case 4: {
+                        System.out.println("GALIL");
+                        saldo -= 960;
+                        break;
+                    }
+                    case 5: {
+                        System.out.println("AK-47");
+                        saldo -= 999;
+                        break;
+                    }
                 }
-                case 2: {
-                    System.out.println("FAMAS");
-                    novosaldo = saldo - 900;
-                    break;
-                }
-                case 3: {
-                    System.out.println("SCAR-H");
-                    novosaldo = saldo - 1050;
-                    break;
-                }
-                case 4: {
-                    System.out.println("GALIL");
-                    novosaldo = saldo - 960;
-                    break;
-                }
-                case 5: {
-                    System.out.println("AK-47");
-                    novosaldo = saldo - 999;
-                    break;
-                }
+                break;
             }
-            break;
-        }
 
-        case 4:{
-            System.out.println("Lightmachine guns");
+            case 4: {
+                System.out.println("Lightmachine guns");
 
-            Scanner var4 = new Scanner(System.in);
-            System.out.println("Produto:");
-            int produtos = var4.nextInt();
+                Scanner var4 = new Scanner(System.in);
+                System.out.println("Produto:");
+                int produtos = var4.nextInt();
 
-            switch (produtos) {
-                case 1: {
-                    System.out.println("Negev");
-                    novosaldo = saldo - 455;
-                    break;
+                switch (produtos) {
+                    case 1: {
+                        System.out.println("Negev");
+                        saldo -= 455;
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("M249");
+                        saldo -= 555;
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("M60");
+                        saldo -= 655;
+                        break;
+                    }
+                    case 4: {
+                        System.out.println("M2");
+                        saldo -= 755;
+                        break;
+                    }
+                    case 5: {
+                        System.out.println("L-SAT");
+                        saldo -= 855;
+                        break;
+                    }
                 }
-                case 2: {
-                    System.out.println("M249");
-                    novosaldo = saldo - 555;
-                    break;
-                }
-                case 3: {
-                    System.out.println("M60");
-                    novosaldo = saldo - 655;
-                    break;
-                }
-                case 4: {
-                    System.out.println("M2");
-                    novosaldo = saldo - 755;
-                    break;
-                }
-                case 5: {
-                    System.out.println("L-SAT");
-                    novosaldo = saldo - 855;
-                    break;
-                }
+                break;
             }
-            break;
-        }
 
-        case 5:{
-            System.out.println("Sniper Rifles");
+            case 5: {
+                System.out.println("Sniper Rifles");
 
-            Scanner var5 = new Scanner(System.in);
-            System.out.println("Produto:");
-            int produtos = var5.nextInt();
+                Scanner var5 = new Scanner(System.in);
+                System.out.println("Produto:");
+                int produtos = var5.nextInt();
 
-            switch (produtos) {
-                case 1: {
-                    System.out.println("AWP");
-                    novosaldo = saldo - 1200;
-                    break;
+                switch (produtos) {
+                    case 1: {
+                        System.out.println("AWP");
+                        saldo -= 1200;
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("SSG");
+                        saldo -= 1025;
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("G3SG1");
+                        saldo -= 1045;
+                        break;
+                    }
+                    case 4: {
+                        System.out.println("Balista");
+                        saldo -= 1400;
+                        break;
+                    }
+                    case 5: {
+                        System.out.println("Kar-98");
+                        saldo -= 1001;
+                        break;
+                    }
                 }
-                case 2: {
-                    System.out.println("SSG");
-                    novosaldo = saldo - 1025;
-                    break;
-                }
-                case 3: {
-                    System.out.println("G3SG1");
-                    novosaldo = saldo - 1045;
-                    break;
-                }
-                case 4: {
-                    System.out.println("Balista");
-                    novosaldo = saldo - 1400;
-                    break;
-                }
-                case 5: {
-                    System.out.println("Kar-98");
-                    novosaldo = saldo - 1001;
-                    break;
-                }
+                break;
             }
-            break;
         }
-    }
+        System.out.println("O seu saldo é :" + saldo);
     }
 }
 
